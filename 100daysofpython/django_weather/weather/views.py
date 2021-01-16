f745cce5009fab544f722b453dcc8ff3eb1b97b9
Prashant Maharjan <prashantmaharjan5@gmail.com>
2021-01-16 21:43:48 +0545

    Update views.py

--- 100daysofpython/django_weather/weather/views.py
+++ 100daysofpython/django_weather/weather/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
-res = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=27.6981&lon=85.3592&exclude=hourly,daily,minutely,alerts&units=metric&appid=dae2adce534d685216e60261c2395f24')
+res = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=27.6981&lon=85.3592&exclude=hourly,daily,minutely,alerts&units=metric&appid={YOURAPIKEY}')
 data = res.json()
 
 def home(request):
